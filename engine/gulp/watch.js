8f8edb1bb8338f88b2f507281269e96fcf072ded
itechdom <idea@itechdom.com>
2015-06-24 10:27:20 -0600

    fixed intial issues with gulp

--- engine/gulp/watch.js
+++ engine/gulp/watch.js
@@ -1,15 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
+var serve = require('browser-sync');
+
 
 module.exports = function(options) {
-gulp.task('watch', function(){
-	var allPaths = [].concat(
-		[paths.js],
-		[paths.all],
-		paths.html,
-		[paths.styl]
-	);
-	gulp.watch(allPaths, ['webpack', reload]);
+gulp.task('watch', function(options){
+	gulp.watch(options.src, ['webpack', serve.reload()]);
 });
 };
